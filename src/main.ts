e2db5e5af4d45faf8b8b6db573ee6faf54d2866f
MaksymvKaspriv <vestry.odessa@gmail.com>
2026-06-20 12:54:17 +0200

    feat: added nestjs logger to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
+  const logger = new Logger('Bootstrap');
 
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow<number>('PORT');
 
   await app.listen(port);
-  console.log(`Application running on port ${port}`);
+  logger.log(`Application running on port ${port}`);
 }
 bootstrap();
